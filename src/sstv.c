fb557d58523af019e947ec34aa0946d1b2983fd9
Jonathan Uhler <jonathan@uhler.com>
2025-02-09 18:02:24 -0800

    transition from make to cmake, add comments to wav_file and deal with non-canonical LIST segment

--- src/sstv.c
+++ src/sstv.c
@@ -3,7 +3,7 @@
 
 
 int main(void) {
-    WavFile *wav_file = wav_file_open("example.wav");
+    WavFile *wav_file = wav_file_open("/Users/jonathan/Downloads/PD120 SSTV Test Recording.wav");
     wav_file_print(wav_file);
     wav_file_print_data(wav_file);
     wav_file_close(wav_file);
--- src/wav_file.c
+++ src/wav_file.c
@@ -14,12 +14,18 @@ WavFile *wav_file_open(const char *path) {
         return NULL;
     }
 
+    // Allocate memory for the wave file data. If the `malloc` calls fail, then we cannot recover,
+    // so we free any allocated memory and exit.
     WavFile *wav_file = (WavFile *) malloc(sizeof(WavFile));
     WavHeader *header = (WavHeader *) malloc(sizeof(WavHeader));
     if (wav_file == NULL || header == NULL) {
+        free(wav_file);
+        free(header);
+        fclose(file);
         return NULL;
     }
 
+    // The basic canonical fields of the riff header are read directly into their struct members.
     fread(header->riff_marker,      sizeof(header->riff_marker),     1, file);
     fread(&header->size,            sizeof(header->size),            1, file);
     fread(header->wave_marker,      sizeof(header->wave_marker),     1, file);
@@ -31,9 +37,21 @@ WavFile *wav_file_open(const char *path) {
     fread(&header->byte_rate,       sizeof(header->byte_rate),       1, file);
     fread(&header->block_align,     sizeof(header->block_align),     1, file);
     fread(&header->bits_per_sample, sizeof(header->bits_per_sample), 1, file);
-    fread(header->data_marker,      sizeof(header->data_marker),     1, file);
-    fread(&header->data_size,       sizeof(header->data_size),       1, file);
 
+    // For the data segment, we must deal with non-canonical riff data. Some files place additional
+    // chunks (e.g. "LIST") immediately before the "data" segment. Each chunk will have a 4-byte
+    // size following it that we can use to skip the chunk. We do this until we find "data"
+    fread(header->data_marker, sizeof(header->data_marker), 1, file);
+    while (strcmp(header->data_marker, "data") != 0) {
+        uint32_t chunk_size = 0;
+        fread(&chunk_size, sizeof(chunk_size), 1, file);
+        fseek(file, chunk_size, SEEK_CUR);
+        fread(header->data_marker, sizeof(header->data_marker), 1, file);
+    }
+
+    fread(&header->data_size, sizeof(header->data_size), 1, file);
+
+    // The rest of the file data is allocated and the samples are read.
     uint8_t *data = (uint8_t *) malloc(header->data_size);
     fread(data, sizeof(uint8_t), header->data_size, file);
 
@@ -44,12 +62,14 @@ WavFile *wav_file_open(const char *path) {
 
 
 void wav_file_print(const WavFile *wav_file) {
+    // Top-level message declaring this is a wave file structure.
     printf("WavFile:\n");
     if (wav_file == NULL) {
         printf("[NULL]\n");
         return;
     }
 
+    // Top-level message for the header data.
     printf(" Header:\n");
     WavHeader *header = wav_file->header;
     if (wav_file->header == NULL) {
@@ -57,6 +77,8 @@ void wav_file_print(const WavFile *wav_file) {
         return;
     }
 
+    // Header data printed out in a human-readable format. For the 'string' types (e.g. char[]),
+    // there are no trailing NUL bytes, so we tell `printf` exactly how many characters to print.
     printf("  RIFF marker: %.4s\n",  header->riff_marker);
     printf("  Size:        %d B\n",  header->size);
     printf("  WAVE marker: %.4s\n",  header->wave_marker);
@@ -74,12 +96,14 @@ void wav_file_print(const WavFile *wav_file) {
 
 
 void wav_file_print_data(const WavFile *wav_file) {
+    // Top-level message declaring this is a wave file structure.
     printf("WavFile:\n");
     if (wav_file == NULL) {
         printf("[NULL]\n");
         return;
     }
 
+    // Top-level message for the list of raw audio samples.
     printf(" Samples:\n");
     WavHeader *header = wav_file->header;
     uint8_t *data = wav_file->data;
@@ -88,17 +112,24 @@ void wav_file_print_data(const WavFile *wav_file) {
         return;
     }
 
+    // We assume a maximum fidelity of 32-bits per sample (`uint32_t`). If this is violated
+    // by the sample size in the header, we cannot print the samples.
     uint32_t sample_size = header->num_channels * header->bits_per_sample / CHAR_BIT;
     uint32_t data_size_per_channel = header->data_size / header->num_channels;
     if (sample_size > sizeof(uint32_t)) {
         printf("  [ERROR: Sample size %d B cannot be greater than 4 B]\n", sample_size);
+        return;
     }
 
+    // For each sample, we loop through each channel (i.e. mono or stereo) and print the hex
+    // information for that sample/channel. For audio data where a sample occupies more than one
+    // byte, we must construct the multi-byte sample
     for (size_t sample = 0; sample < data_size_per_channel; sample += sample_size) {
         printf("  Sample %8zu:", sample);
         for (size_t channel = 0; channel < header->num_channels; channel++) {
             uint32_t sample_data = 0;
 
+            // Construct the multi-byte sample
             for (size_t byte_in_sample = 0; byte_in_sample < sample_size; byte_in_sample++) {
                 uint32_t byte_data = data[sample + byte_in_sample];
                 byte_data <<= byte_in_sample * CHAR_BIT;
